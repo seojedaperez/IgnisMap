4629a65e1514313fe9cd2787fd452149f587f102
Santiago Ezequiel Ojeda Perez <seojedaperez@gmail.com>
2025-06-30 22:39:29 -0300

    Fix 404 errors and base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/IgnisMap-main/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/IgnisMap/' : '/',
   plugins: [
     react(),
     VitePWA({
